8a3da295297f4447b09eec7d92b22353ce9c37c4
Anthony <anthonyhunter2@hotmail.com>
2017-12-06 23:47:54 -0500

    this worked

--- day6_pt1.py
+++ day6_pt1.py
@@ -18,21 +18,24 @@ def loopedlist(stuff,times, ind):
 			ind = -1 
 	return(stuff)
 list1 = file_guy()
+#list2 = loopedlist(file_guy(), 14,5)
 list2 = file_guy()
 firstpoint = list2.index(max(list2)) 
 def main(guy, test, sp):
 	count = 0
 	startingpoint = None
 	new_guy = []
+	done_al = []
 	#while compare != test:# and startingpoint != sp:
 	while count < 500000:
-		if "".join(str(x) for x in new_guy) == "".join(str(x) for x in test):
-			break
+		startingpoint = guy.index(max(guy))
+		distrib = guy[guy.index(max(guy))]	
+		new_guy = loopedlist(guy, distrib, startingpoint)
+		count += 1	
+		if "".join(str(x) for x in new_guy) not in done_al:
+			done_al.append("".join(str(x) for x in new_guy))
 		else:
-			startingpoint = guy.index(max(guy))
-			distrib = guy[guy.index(max(guy))]	
-			new_guy = loopedlist(guy, distrib, startingpoint)
-			count += 1	
+			break
 	return(count)
 print(main(list1, list2, firstpoint))
 
