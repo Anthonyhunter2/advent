735a5dc5b1258b5555fb518a89a3f40f6306f487
Anthony <anthonyhunter2@hotmail.com>
2017-12-10 14:55:08 -0500

    Cleaned up a bit

--- day8_pt1.py
+++ day8_pt1.py
@@ -8,39 +8,23 @@ def ofile():
 		for line in f:
 			line = line.split()
 			full.append(line)
-#			var = line[0]
-#			if_state = " ".join(line[3:]) + ":"
-#			step = line[2]
-#			if line[1] == "inc":
-#				action = "+ {}".format(step)
-#			elif line[1] == "dec":
-#				action = "- {}".format(step)
-#			else:
-#				print("Didnt understand what \"{}\" was".format(line[1]))
-#			if_state = "{} {} {}".format(" ".join(line[3:]) + ":", var, action) 
-#			add = var, if_state
-#			full.append(add)
 		return(full)
-def check_it(check, stuff):
-	print(check)
-	var1 = check[0]
-	var2 = check[4]
-	if var2 in stuff:
-		checker = "{} {} {}".format(stuff[var2], check[5], int(check[6]))
-		if eval(checker):
-			if check[1] == "inc":
-				stuff[var1] = stuff[var1] + int(check[2])
-			elif check[1] == "dec":
-				stuff[var1] = stuff[var1] - int(check[2])
-		else:
-			pass
-	return(stuff)
-	
 def main(guy):
 	vals_dict = {}
 	for line in guy:
 		vals_dict[line[0]] = 0
 	for line in guy:
-		check_it(line, vals_dict)
+	#	check_it(line, vals_dict)
+		var2 = line[4]
+		var1 = line[0]
+		if var2 in vals_dict:
+			checker = "{} {} {}".format(vals_dict[var2], line[5], int(line[6]))
+			if eval(checker):
+				if line[1] == "inc":
+					vals_dict[var1] = vals_dict[var1] + int(line[2])
+				elif line[1] == "dec":
+					vals_dict[var1] = vals_dict[var1] - int(line[2])
+			else:
+				pass
 	print(max(vals_dict.values()))
 main(ofile())
