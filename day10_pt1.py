f8a377f1346b89703110e40ac40ed4acabe1365f
Anthony <anthonyhunter2@hotmail.com>
2017-12-16 10:52:07 -0500

    syncing up

--- day10_pt1.py
+++ day10_pt1.py
@@ -9,40 +9,59 @@ def ofile():
             i = int(i)
             new_list.append(i)
         return(new_list)
-def loopedlist(w_list, jabby, w_ind, j_ind):
-#    times = jabby[j_ind]
-    rev_list = []
-    ind1 = w_list[w_ind]
-    ind2 = jabby[j_ind]
-    for a in reversed(w_list[w_ind:w_list[w_ind + jabby[j_ind]]]):
-        rev_list.append(a)
-    print(rev_list)
-    for i in w_list[w_ind:w_list[w_ind + jabby[j_ind]]]:
-        print(i)
- #   while times > 0:
-  #      try:
-   #         
-    #        ind = ind + 1
-    #        stuff[ind] = stuff[ind] +1
-    #        times -= 1
-    #    except IndexError:
-    #        ind = -1
-   # return(stuff)
-    
+def loopedlist(w_list, w_ind, j_ind):
+    print(j_ind - 1, "IM TXT")
+    print(w_list, "HI, STILL HRE")
+   # times = (j_ind + 1)
+    # times2 = j_ind
+    # start_list = []
+    # rev_list = []
+    # start_list.append(w_list[w_ind])
+    # next_one = w_ind
+    # next_two = w_ind
+    # print(times)
+    # while times > 0:
+    #     try:
+    #         next_one = next_one + 1
+    #         start_list.append(w_list[next_one])
+    #         times -= 1
+    #     except IndexError:
+    #         next_one = -1
+    # for i in reversed(start_list):
+    #     rev_list.append(i)
+    # print(rev_list)
+    # for x in range(len(rev_list)):
+    #     try:
+    #         w_list[next_two] = rev_list[x]
+    #         next_two = next_two + 1
+    #     except IndexError:
+    #         next_two = 0
+    #         w_list[next_two] = rev_list[x]
+    #         next_two = next_two + 1
+    # return(w_list)
 def main(jabby):
     working_list = []
     list_for_changes = []
-    index_num_step = 0
-    index_num_list = 1
+    index_num_list = 0
     step = 0
-    for each in range(0,5):
+    cycles = len(jabby)
+    for each in range(0,10):
         working_list.append(each)
-    print(working_list, index_num_list, jabby, index_num_step)
-    print(working_list[index_num_list], jabby[index_num_step])
-    loopedlist(working_list, jabby, index_num_list, index_num_step)
-#    print(reversed(working_list[index_num_list:jabby[index_num_step]]))
-   # for a in reversed(working_list[index_num_list:jabby[index_num_step]]):
-    #    print(a)
-   # for a in working_list[index_num_list:jabby[index_num_step]+1]:
+   # while cycles > 0:
+    for x in range(len(jabby)):
+        length = int(jabby[x])
+     #   cwp = index_num_list
+   #     index_num_list = length + step
+        print(index_num_list, working_list[index_num_list], "TAG")
+        if working_list[index_num_list]:
+            print("BOUT TO RUN")
+            loopedlist(working_list, index_num_list, length)
+   #     else:
+    #        index_num_list = index_num_list - working_list[cwp:]
+        print(working_list, index_num_list, jabby, length)
+        print(working_list[index_num_list], length)
+   #     index_num_list += 1
+    #    step += 1
+    #print(loopedlist(working_list, jabby, index_num_list, index_num_step))
 main(ofile())
 
