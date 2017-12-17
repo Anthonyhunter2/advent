09b3fcbc363706fda0099e845e5bc9587f4bb2ef
Anthony <anthonyhunter2@hotmail.com>
2017-12-17 09:03:34 -0500

    finished day10_pt1

--- archive/day10_pt1.py
+++ archive/day10_pt1.py
@@ -4,21 +4,19 @@ def ofile():
     with open("steps","r") as f:
         new_list = []
         f = f.readline()   
-        for i in f.split():
-            i = i.strip(",").strip("\n")
+        for i in f.split(","):
+            i = i.strip("\n")
             i = int(i)
             new_list.append(i)
         return(new_list)
-def loopedlist(w_list, w_ind, j_ind):
-    print(j_ind - 1, "IM TXT")
-    times = (j_ind + 1)
+def loopedlist(w_list, w_ind, j_ind, jabby):
+    times = (j_ind - 1)
     times2 = j_ind
     start_list = []
     rev_list = []
     start_list.append(w_list[w_ind])
     next_one = w_ind
     next_two = w_ind
-    print(times)
     while times > 0:
         try:
             next_one = next_one + 1
@@ -28,7 +26,6 @@ def loopedlist(w_list, w_ind, j_ind):
             next_one = -1
     for i in reversed(start_list):
         rev_list.append(i)
-    print(rev_list)
     for x in range(len(rev_list)):
         try:
             w_list[next_two] = rev_list[x]
@@ -38,26 +35,34 @@ def loopedlist(w_list, w_ind, j_ind):
             w_list[next_two] = rev_list[x]
             next_two = next_two + 1
     return(w_list)
+
+def where_am_i (lister, ind, length, step):
+    count = length + step
+    while count > 0:
+        try:
+            ind = ind + 1
+            lister[ind]
+            count -= 1
+        except IndexError:
+            ind = -1
+    return(ind)
 def main(jabby):
     working_list = []
-    list_for_changes = []
     index_num_list = 0
     step = 0
-    cycles = len(jabby)
-    for each in range(0,10):
+    for each in range(0,256):
         working_list.append(each)
-   # while cycles > 0:
     for x in range(len(jabby)):
         length = int(jabby[x])
-     #   cwp = index_num_list
-   #     index_num_list = length + step
-        print(index_num_list, working_list[index_num_list], length)
-        loopedlist(working_list, index_num_list, length)
-   #     else:
-    #        index_num_list = index_num_list - working_list[cwp:]
-        print(working_list, index_num_list, jabby, length)
-        print(working_list[index_num_list], length)
-   #     index_num_list += 1
-    #    step += 1
+        loopedlist(working_list, index_num_list, length, jabby)
+        index_num_list = where_am_i(working_list, index_num_list, length, step)
+        step += 1
+    #    print(working_list)
+    #    print(index_num_list, "index")
+    print(working_list)
+    print(working_list[0] * working_list[1])
+        
+            
+        
 main(ofile())
 
